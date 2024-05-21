6cdc77eab2d6ffa794fb425f23090daf9038d677
jinwoo park <swat018@gmail.com>
2024-05-21 17:48:43 +0900

    fix : 연결된 데이터 URL 정보를 환경변수로 빼기

--- src/api/aisData.js
+++ src/api/aisData.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 // const baseUrl = 'http://navioncorp.asuscomm.com:58000/data'
-const baseUrl = import.meta.env.AIS_DATA_URL
+const baseUrl = import.meta.env.VITE_AIS_DATA_URL
 
-import.meta.env.TILE_MAP_URL
 export const getAisData = () => {
   return axios.get(baseUrl).then((response) => {
     if (response.data.length != 0) {
